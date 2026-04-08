71e0157e26f747c73a4862e54d7822cd7be5a301
Miguel Angelo Basinillo <miguel102104@gmail.com>
2026-04-08 12:43:07 +0800

    Part 5: Input and Properties

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,10 +1,34 @@
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "../layout/AppLayout";
+import FloatingLabelInput from "../components/Input/FloatingLabelInput";
+import { useState } from "react";
 
 const SampleComponent = () => {
+  const [firstName,  setFirstName] = useState("");
+  const [lastName,  setLastName] = useState("");
+  const [birthDate,  setBirthDate] = useState("");
+  const [password,  setPassword] = useState("");
+
     return (
         <>
-            <h1 className="text-red-600">Welcome</h1>
+            <h1 className="text-red-600 mb-2 ">Welcome!</h1>
+            <p className="text-gray-600 mb-8 text-sm italic">Please enter your details below:</p>
+            <div className="mb-4">
+            <FloatingLabelInput label="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" name="first_name" required autoFocus />
+            <p className="font-medium mt-2">First Name: {firstName}</p>
+            </div>
+            <div className="mb-4">
+            <FloatingLabelInput label="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" name="last_name" required />
+            <p className="font-medium mt-2">Last Name: {lastName}</p>
+            </div>
+            <div className="mb-4">
+            <FloatingLabelInput label="Birth Date" value={birthDate} onChange={(e) => setBirthDate(e.target.value)} type="date" name="birth_date" />
+            <p className="font-medium mt-2">Birth Date: {birthDate}</p>
+            </div>
+            <div className="mb-4">
+            <FloatingLabelInput label="Password" value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" />
+            <p className="font-medium mt-2">Password: {password}</p>
+            </div>
         </>
     )
 }
