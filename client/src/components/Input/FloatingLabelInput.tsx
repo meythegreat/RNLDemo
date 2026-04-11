5eb126771da1a51659ed31954d8b9bf54f0f708c
Miguel Angelo Basinillo <miguel102104@gmail.com>
2026-04-11 08:55:05 +0800

    Fix floating label background overflow in input field

--- client/src/components/Input/FloatingLabelInput.tsx
+++ client/src/components/Input/FloatingLabelInput.tsx
@@ -57,7 +57,7 @@ const FloatingLabelInput: FC<FloatingLabelInputProps> = ({
           className={`${
             newLabelClassName
               ? newLabelClassName
-              : `absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-left bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto inset-1 ${labelClassName}`
+              : `absolute inline-block text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-left bg-white px-1 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto left-2 ${labelClassName}`
           }`}
         >
           {label}
